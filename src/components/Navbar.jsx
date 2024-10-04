fe6d90c45926093063ae4ac3a8991a4f016ed0b7
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-10-04 18:20:35 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <div className='nav flex items-center justify-between text-sm py-4  mb-5  border-b border-b-gray-400'>              
       <img className=' ic cursor-pointer' src='/logo.png' alt=" logo" />
-      <h3 className='Name'>Hospital Management</h3>
+      <h1 className='Name'>Hospital Management</h1>
         <ul className='hidden md:flex items-start gap-5 font-medium'>
-            <NavLink>
+            <NavLink to='/'>
                 <li className='navigation py 1'>Home</li>
                 <hr className='border-none outline-none h-0.5  m-auto' />
             </NavLink>
            
-            <NavLink>
+            <NavLink to='/about-us'>
                 <li className='navigation py 1'>About Us</li>
                 <hr className='border-none outline-none h-0.5 m-auto' />
             </NavLink>
