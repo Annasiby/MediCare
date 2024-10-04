593e43cec43e7da8b612826c4f4ad5acecf4a141
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-10-04 18:22:58 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='roles '>
+      <p className="About">
+      Our Hospital Management System is designed to streamline hospital operations and enhance patient care. With advanced features like patient management, doctor scheduling, billing, and real-time data access, we aim to simplify healthcare processes for hospitals, clinics, and medical centers. Our platform prioritizes efficiency, security, and a seamless experience for both healthcare providers and patients. 
+      <br></br>
+      </p>
+      <h1>Choose your role</h1>
+       <div className="role-choose ">
+        <div className="role-card">
+          <img className=' role cursor-pointer' src="/admin.png" alt="Admin" />
+          <button className="role-btn">Admin</button>
+        </div>
+        <div className="role-card">
+          <img className='role cursor-pointer'src="/patient.png" alt="Patient" />
+          <button className="role-btn">Patient</button>
+        </div>
+        <div className="role-card">
+          <img className='role cursor-pointer'src="/doctor.png" alt="Doctor" />
+          <button className="role-btn">Doctor</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
